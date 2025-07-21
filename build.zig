e4cf3d5b3d78db96084fba71c907181d90f8d20f
EnterAUsername4 <93722635+EnterAUsername4@users.noreply.github.com>
2025-07-21 12:02:00 +0100

    Update build.zig (use zig version 0.9.0)

--- build.zig
+++ build.zig
@@ -14,41 +14,43 @@ pub fn build(b: *std.build.Builder) void {
     const arch = b.option(ClumsyArch, "arch", "x86, x64") orelse .x64;
     const conf = b.option(ClumsyConf, "conf", "Debug, Release") orelse .Debug;
     const windivert_sign = b.option(ClumsyWinDivertSign, "sign", "A, B, C") orelse .A;
-    const windows_kit_bin_root = b.option([]const u8, "windows_kit_bin_root", "Windows SDK Bin root") orelse "C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0";
+    const windows_kit_bin_root = b.option([]const u8, "windows_kit_bin_root", "Windows SDK Bin root") orelse "C:/Program Files (x86)/Windows Kits/10/bin/10.0.26100.0";
 
     const arch_tag = @tagName(arch);
     const conf_tag = @tagName(conf);
     const sign_tag = @tagName(windivert_sign);
-    const windivert_dir = b.fmt("WinDivert-2.2.0-{s}", .{sign_tag});
+    const windivert_dir = b.fmt("WinDivert-2.2.2-{s}", .{sign_tag});
 
-    debug.print("- arch: {s}, conf: {s}, sign: {s}\n", .{@tagName(arch), @tagName(conf), @tagName(windivert_sign)});
+    debug.print("- arch: {s}, conf: {s}, sign: {s}\n", .{ @tagName(arch), @tagName(conf), @tagName(windivert_sign) });
     debug.print("- windows_kit_bin_root: {s}\n", .{windows_kit_bin_root});
     _ = std.fs.realpathAlloc(b.allocator, windows_kit_bin_root) catch @panic("windows_kit_bin_root not found");
 
-    const prefix = b.fmt("{s}_{s}_{s}", .{arch_tag, conf_tag, sign_tag});
-    b.exe_dir = b.fmt("{s}/{s}", .{b.install_path, prefix});
+    const prefix = b.fmt("{s}_{s}_{s}", .{ arch_tag, conf_tag, sign_tag });
+    b.exe_dir = b.fmt("{s}/{s}", .{ b.install_path, prefix });
 
     debug.print("- out: {s}\n", .{b.exe_dir});
 
     const tmp_path = b.fmt("tmp/{s}", .{prefix});
     b.makePath(tmp_path) catch @panic("unable to create tmp directory");
 
-    b.installFile(b.fmt("external/{s}/{s}/WinDivert.dll", .{windivert_dir, arch_tag}), b.fmt("{s}/WinDivert.dll", .{prefix}));
+    b.installFile(b.fmt("external/{s}/{s}/WinDivert.dll", .{ windivert_dir, arch_tag }), b.fmt("{s}/WinDivert.dll", .{prefix}));
     switch (arch) {
-        .x64 => b.installFile(b.fmt("external/{s}/{s}/WinDivert64.sys", .{windivert_dir, arch_tag}), b.fmt("{s}/WinDivert64.sys", .{prefix})),
-        .x86 => b.installFile(b.fmt("external/{s}/{s}/WinDivert32.sys", .{windivert_dir, arch_tag}), b.fmt("{s}/WinDivert32.sys", .{prefix})),
+        .x64 => b.installFile(b.fmt("external/{s}/{s}/WinDivert64.sys", .{ windivert_dir, arch_tag }), b.fmt("{s}/WinDivert64.sys", .{prefix})),
+        .x86 => b.installFile(b.fmt("external/{s}/{s}/WinDivert32.sys", .{ windivert_dir, arch_tag }), b.fmt("{s}/WinDivert32.sys", .{prefix})),
     }
 
     b.installFile("etc/config.txt", b.fmt("{s}/config.txt", .{prefix}));
     if (conf == .Ship)
         b.installFile("LICENSE", b.fmt("{s}/License.txt", .{prefix}));
 
+    b.installFile("etc/keybinds.txt", b.fmt("{s}/keybinds.txt", .{prefix}));
+
     const res_obj_path = b.fmt("{s}/clumsy_res.obj", .{tmp_path});
 
     const rc_exe = b.findProgram(&.{
         "rc",
     }, &.{
-        b.pathJoin(&.{windows_kit_bin_root, @tagName(arch)}),
+        b.pathJoin(&.{ windows_kit_bin_root, @tagName(arch) }),
     }) catch @panic("unable to find `rc.exe`, check your windows_kit_bin_root");
 
     const archFlag = switch (arch) {
@@ -84,7 +86,7 @@ pub fn build(b: *std.build.Builder) void {
             exe.subsystem = .Windows;
         },
     }
-    const triple  = switch (arch) {
+    const triple = switch (arch) {
         .x64 => "x86_64-windows-gnu",
         .x86 => "i386-windows-gnu",
     };
@@ -101,6 +103,7 @@ pub fn build(b: *std.build.Builder) void {
     exe.addCSourceFile("src/drop.c", &.{""});
     exe.addCSourceFile("src/duplicate.c", &.{""});
     exe.addCSourceFile("src/elevate.c", &.{""});
+    exe.addCSourceFile("src/crashlog.c", &.{""});
     exe.addCSourceFile("src/lag.c", &.{""});
     exe.addCSourceFile("src/main.c", &.{""});
     exe.addCSourceFile("src/ood.c", &.{""});
@@ -109,7 +112,8 @@ pub fn build(b: *std.build.Builder) void {
     exe.addCSourceFile("src/tamper.c", &.{""});
     exe.addCSourceFile("src/throttle.c", &.{""});
     exe.addCSourceFile("src/utils.c", &.{""});
-    exe.addCSourceFile("src/utils.c", &.{""});
+    exe.addCSourceFile("src/pipe.c", &.{""});
+    exe.addCSourceFile("src/disconnect.c", &.{""});
 
     if (arch == .x86)
         exe.addCSourceFile("etc/chkstk.s", &.{""});
@@ -117,15 +121,19 @@ pub fn build(b: *std.build.Builder) void {
     exe.addIncludeDir(b.fmt("external/{s}/include", .{windivert_dir}));
 
     const iupLib = switch (arch) {
-        .x64 => "external/iup-3.30_Win64_mingw6_lib",
-        .x86 => "external/iup-3.30_Win32_mingw6_lib",
+        .x64 => "external/iup-3.32_Win64_mingw6_lib",
+        .x86 => "external/iup-3.32_Win32_mingw6_lib",
     };
 
-    exe.addIncludeDir(b.pathJoin(&.{iupLib, "include"}));
-    exe.addCSourceFile(b.pathJoin(&.{iupLib, "libiup.a"}), &.{""});
+    exe.addIncludeDir(b.pathJoin(&.{ iupLib, "include" }));
+    exe.addCSourceFile(b.pathJoin(&.{ iupLib, "libiup.a" }), &.{""});
+
+    //    exe.addLibPath("external/iup-3.32_Win64_dll16_lib");
+    //    exe.linkSystemLibrary("iupcontrols");
+    //    exe.linkSystemLibrary("iupcd");
 
     exe.linkLibC();
-    exe.addLibPath(b.fmt("external/{s}/{s}", .{windivert_dir, arch_tag}));
+    exe.addLibPath(b.fmt("external/{s}/{s}", .{ windivert_dir, arch_tag }));
     exe.linkSystemLibrary("WinDivert");
     exe.linkSystemLibrary("comctl32");
     exe.linkSystemLibrary("Winmm");
@@ -135,16 +143,15 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkSystemLibrary("comdlg32");
     exe.linkSystemLibrary("uuid");
     exe.linkSystemLibrary("ole32");
+    exe.linkSystemLibrary("dbghelp");
+    //    exe.subsystem = .Windows;
 
-    const exe_install_step = b.addInstallArtifact(exe);  
-    if (conf == .Ship)
-    {
+    const exe_install_step = b.addInstallArtifact(exe);
+    if (conf == .Ship) {
         const remove_pdb_step = RemoveOutFile.create(b, "clumsy.pdb");
         remove_pdb_step.step.dependOn(&exe_install_step.step);
         b.getInstallStep().dependOn(&remove_pdb_step.step);
-    }
-    else
-    {
+    } else {
         b.getInstallStep().dependOn(&exe_install_step.step);
     }
 
@@ -161,7 +168,7 @@ pub const RemoveOutFile = struct {
 
     pub fn create(builder: *Builder, rel_path: []const u8) *@This() {
         const self = builder.allocator.create(@This()) catch unreachable;
-        self.* = . {
+        self.* = .{
             .step = Step.init(.custom, builder.fmt("RemoveOutFile {s}", .{rel_path}), builder.allocator, make),
             .builder = builder,
             .rel_path = rel_path,
